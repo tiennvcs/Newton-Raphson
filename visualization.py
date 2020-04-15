3f95e34d36f99c9b382e09860d4fea618ddefa00
tiennvuit <tiennvuit@gmail.com>
2020-04-15 17:04:07 +0700

    'plot'

--- visualization.py
+++ visualization.py
@@ -1,6 +1,12 @@
-from main import *
+import argparse
+import subprocess
+import numpy as np
+from main import newton_raspson
+from matplotlib import pyplot as plt
+from main import getExpectation
+from datetime import datetime
 
-def plotGraph(solutions: np.ndarray, iterators: np.ndarray, errors: np.ndarray, ERs: list, T2: float):
+def plotGraph(expectations: np.ndarray, iterators: np.ndarray, errors: np.ndarray, ERs: list, T2: float):
 	"""
 	@parameters:
 		- solutions is a matrix that contain all solution vectors of system of equations.
@@ -10,43 +16,61 @@ def plotGraph(solutions: np.ndarray, iterators: np.ndarray, errors: np.ndarray, 
 	@return values:
 		- None
 	"""
-	plt.style.use('seaborn')
+	plt.style.use('seaborn-deep')
 	fig, ax = plt.subplots(figsize=(9,6))
 
-	ax.plot(ERs, solutions[:, 0], color='b')
-	ax.plot(ERs, solutions[:, 1], color='g')
-	ax.plot(ERs, solutions[:, 2], color='r')
-	ax.plot(ERs, solutions[:, 3], color='c')
-	ax.plot(ERs, solutions[:, 4], color='m')
-	ax.plot(ERs, solutions[:, 5], color='y')
+	ax.plot(ERs, expectations[:, 0], color='b', label='C')
+	ax.plot(ERs, expectations[:, 1], color='g', label='H2')
+	ax.plot(ERs, expectations[:, 2], color='r', label='CO')
+	ax.plot(ERs, expectations[:, 3], color='c', label='CO2')
+	ax.plot(ERs, expectations[:, 4], color='y', label='CH4')
 
 	ax.set_xlabel('ER', fontsize=14)
-	ax.set_ylabel('y', fontsize=14)
-	ax.set_title('Oservation when ER change', fontsize=20)
+	ax.set_ylabel('Syngas Compositions(%)', fontsize=14)
+	ax.set_title('Effect when changing ER', fontsize=20)
+	legend = ax.legend(shadow=True, fontsize='x-large')
+	legend.get_frame().set_facecolor('C1')
+	plt.xticks(ticks=ERs)
 	plt.xlim(0.19, np.max(ERs)+1e-4)
-	plt.ylim(0, np.max(solutions)+1)
+	plt.ylim(0, np.max(expectations)+1)
+
 	plt.show()
+
+	while True:
+		key = input("Do you want to save the figure (Yes/No)? ")
+		if key.upper() == "YES" or key.upper() == 'Y':
+			now = datetime.now()
+			dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
+			fig.savefig('output/' + dt_string + '.jpeg')
+			exit(0)
+		elif key.upper() == "NO" or key.upper() == "N":
+			exit(0)
 
 
 def main(args):
-    T2 = float(args.T2)
-    N = int(args.N)
-    x = args.init_values
-    epxilon = args.epxilon
+	T2 = args.T2
+	ERs = [0.2, 0.25, 0.3, 0.35, 0.4]
+	expectations = np.array([])
+	iterators = np.array([])
+	errors = np.array([])
+	for ER in ERs:
+		running = subprocess.run(['python3', 'main.py', '-ER', '0.3', '-T2', '900'], stdout=subprocess.PIPE)
+		output = running.stdout.decode('utf-8').split("\n")[0].split(" ")
+		solution = np.array(output[0:6], np.float32)
+		n = int(output[6])
+		error = float(output[7])
 
-    solutions = np.array([[]])
-    iterators = np.array([])
-    errors = np.array([])
-    ERs = np.arange(0.2, 0.41, 0.01)
-    for ER in ERs:
-        solution, iterator, error = newton_raspson(x=x, epxilon=epxilon, N=N)
-        solutions = np.append(solutions, solution)
-        iterators = np.append(iterators, iterator)
-        errors = np.append(errors, error)
+		expectation = getExpectation(solution=solution)
+		expectations = np.append(expectations, expectation)
+		iterators = np.append(iterators, n)
+		errors = np.append(errors, error)
 
-    solutions = solutions.reshape(int(len(solutions)/6), 6)
-    plotGraph(solutions=solutions, iterators=iterators, errors=errors, ERs=ERs, T2=T2)
+	expectations = expectations.reshape(int(len(expectations)/5), 5)
+	plotGraph(expectations=expectations, iterators=iterators, errors=errors, ERs=ERs, T2=T2)
+
 
 if __name__ == "__main__":
-    args = getArguments()
-    main(args)
+	parser = argparse.ArgumentParser("Visualize result")
+	parser.add_argument("-T2", help="The value T2", default=750)
+	args = parser.parse_args()
+	plot = main(args)
