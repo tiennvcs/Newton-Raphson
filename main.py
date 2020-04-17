ef084e8bcadc7470da9b4ae5df648f82b0d65ab0
tiennvuit <tiennvuit@gmail.com>
2020-04-17 19:10:19 +0700

    structure code

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import argparse
 import numpy as np
 from numpy import linalg as LA
-from functions import F
-from derivatives import Jacobian
+from parameters import *
+
+def F(x: np.array):
+    f1 = x[0] + x[2] + x[3] + x[5] - 1
+    f2 = 2*x[1] + 2*x[4] + 4*x[5] - a - 2*(w+q)
+    f3 = x[2] + 2*x[3] + x[4] - (b + w + q + 2*m)
+    f4 = K1 * x[1]**2 - x[5]
+    f5 = K2 * x[2]*x[4] - x[1]*x[3]
+    f6 = dH_H2 * x[1] + dH_CO * x[2] + dH_CO2 * x[3] + dH_H2O_k * x[4] + dH_CH4 * x[5] + 3.76*m*dH_N2 - dH_trau - w*dH_H2O_l
+    return np.array([f1, f2, f3, f4, f5, f6])
+
+
+def Jacobian(x):
+    df1 =  np.array([1, 0, 1, 1, 0, 1])
+    df2 = np.array([0, 2, 0, 0, 2, 4])
+    df3 = np.array([0, 0, 1, 2, 1, 0])
+    df4 = np.array([0, 2*K1*x[1], 0, 0, 0, -1])
+    df5 = np.array([0, -x[3], K2*x[4], -x[1], K2*x[2], 0])
+    df6 = np.array([0, dH_H2, dH_CO, dH_CO2, dH_H2O_k, dH_CH4])
+    return np.array([df1, df2, df3, df4, df5, df6])
+
 
 def getArguments():
 	parser = argparse.ArgumentParser(description="Solve equation system using Newton - Raspson")
@@ -13,7 +32,7 @@ def getArguments():
 			help="Nghiệm khởi tạo trước khi chạy thuật toán",
 			default=np.array(np.random.rand(6)))
 
-	parser.add_argument("-epxilon", help="The accuracy of method", default=1e-4)
+	parser.add_argument("-epsilon", help="The accuracy of method", default=1e-4)
 	parser.add_argument("-N", help="Số lượng vòng lặp giới hạn", default=1000)
 	args = parser.parse_args()
 	return args
@@ -34,18 +53,9 @@ def getExpectation(solution: np.ndarray):
 	percent6 = (solution[5] / sum_solution) * 100
 	return np.array([percent1, percent2, percent3, percent4, percent6])
 
-def newton_raspson(x: np.ndarray, epxilon=1e-4, N=1000):
-	"""
-	@parameters:
-		- x (np.array): the initial solution vector when run Newton - Raspson method.
-		- epxilon (float): the accuracy determine when we stop the alogithms.
-	@return values:
-		- An array contains all root of equation system.
-		- A number of iterators for each value.
-	"""
+def newton_raspson(x: np.ndarray, epsilon=1e-6, N=1000):
 	n = 1
-	y = np.ones(6)
-	while (n < N): # Use l2 norm
+	while (n < N): 
 		# Calculate the F(x)
 		Fx = F(x=x)
 		# Calculate the Jacobian matrix
@@ -54,25 +64,27 @@ def newton_raspson(x: np.ndarray, epxilon=1e-4, N=1000):
 		y = solveLinearSystem(jacobi=jacobi, Fx=Fx)
 		# Update solution
 		x = x - y
+
+		# Customize for the problem
 		for i in range(6):
 				if x[i] < 0:
 					x[i] = np.random.rand()
 
-		if LA.norm(y, ord=None) <= epxilon:
-			return {'x':x, 'n': n, 'error': LA.norm(y, ord=None), 'success': 0}
+		if LA.norm(y, ord=None) <= epsilon:
+			return {'x':x, 'n': n, 'error': LA.norm(y, ord=None), 'success': 1}
 		n = n + 1
 
-	return {'x': x, 'n': n, 'error': LA.norm(y, ord=None), 'success': 1}
+	return {'x': x, 'n': n, 'error': LA.norm(y, ord=None), 'success': 0}
 
 
 def main(args):
 	ER = float(args.ER)
 	T2 = float(args.T2)
 	x = args.init_values
-	epxilon = float(args.epxilon)
+	epsilon = float(args.epsilon)
 	N = int(args.N)
 
-	result = newton_raspson(x=x, epxilon=epxilon, N=N)
+	result = newton_raspson(x=x, epsilon=epsilon, N=N)
 	print(
             result['x'][0], result['x'][1], result['x'][2], result['x'][3], result['x'][4], result['x'][5],
             result['n'],
